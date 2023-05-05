b4e31e2261327549dd0cb841a62ffad765ea1353
Giuseppe Fanuli <pepperide@gmail.com>
2023-05-06 00:20:13 +0200

    Added missing import Product controller

--- server/src/main/kotlin/wa2/polito/it/letduchidegliabruzzi/server/product/ProductController.kt
+++ server/src/main/kotlin/wa2/polito/it/letduchidegliabruzzi/server/product/ProductController.kt
@@ -4,6 +4,7 @@ import jakarta.validation.Valid
 import jakarta.validation.constraints.Email
 import jakarta.validation.constraints.NotBlank
 import jakarta.validation.constraints.NotNull
+import org.springframework.http.HttpStatus
 import org.springframework.validation.BindingResult
 import org.springframework.validation.annotation.Validated
 import org.springframework.web.bind.annotation.*

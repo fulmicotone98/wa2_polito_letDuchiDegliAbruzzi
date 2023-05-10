7c066e15ccd51926847958ac5706e94ffe82c2a4
Marta Corcione <marta.corcione@orbyta.it>
2023-05-10 23:19:15 +0200

    fixed warnings

--- server/src/test/kotlin/wa2/polito/it/letduchidegliabruzzi/server/ServerApplicationTests.kt
+++ server/src/test/kotlin/wa2/polito/it/letduchidegliabruzzi/server/ServerApplicationTests.kt
@@ -1,8 +1,7 @@
 package wa2.polito.it.letduchidegliabruzzi.server
 
-import junit.framework.TestCase.*
-import org.junit.jupiter.api.Test
-import org.junit.jupiter.api.Assertions
+import org.junit.Assert.*
+import org.junit.Test
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase
 import org.springframework.boot.test.context.SpringBootTest

937b50c7c3e6e94930e1ab5917517fec55125255
fulmicotone98 <jacopo.catalano98@gmail.com>
2023-05-22 09:23:24 +0200

    feature: Security Filter Chain added in WebSecurityConfig.kt, request matchers should be modified.

--- server/src/main/kotlin/wa2/polito/it/letduchidegliabruzzi/server/security/JwtAuthConverterProperties.kt
+++ server/src/main/kotlin/wa2/polito/it/letduchidegliabruzzi/server/security/JwtAuthConverterProperties.kt
@@ -4,8 +4,6 @@ import org.springframework.boot.context.properties.ConfigurationProperties
 import org.springframework.context.annotation.Configuration
 import org.springframework.validation.annotation.Validated
 
-
-
 @Validated
 @Configuration
 @ConfigurationProperties(prefix = "jwt.auth.converter")

c886932006a8898ca26f4c20baaa159ccb7b6873
Marta Corcione <marta.corcione@orbyta.it>
2023-05-24 12:15:16 +0200

    moved security controller to controllers package

--- server/src/main/kotlin/wa2/polito/it/letduchidegliabruzzi/server/controller/AuthenticationController.kt
+++ server/src/main/kotlin/wa2/polito/it/letduchidegliabruzzi/server/controller/AuthenticationController.kt
@@ -1,9 +1,12 @@
-package wa2.polito.it.letduchidegliabruzzi.server.security
+package wa2.polito.it.letduchidegliabruzzi.server.controller
 
 import org.springframework.http.HttpStatus
 import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RestController
+import wa2.polito.it.letduchidegliabruzzi.server.security.AuthenticationService
+import wa2.polito.it.letduchidegliabruzzi.server.security.Credentials
+import wa2.polito.it.letduchidegliabruzzi.server.security.JwtResponse
 
 @RestController
 class AuthenticationController(private val authenticationService: AuthenticationService) {
